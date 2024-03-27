c459597908ccb1fff8cdf38a4f5f4b15a2eb1e7c
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2024-03-27 11:17:36 +0900

    퍼그 파일 path설정

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+
+app.get("/", (req, res) => res.render("content/home.pug"));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
