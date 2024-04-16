165e68e0128768363667f94302d09b0943176be5
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2024-04-16 18:38:02 +0900

    에러메시지파일 생성 및 관련 코드 수정

--- src/model/searchModel.js
+++ src/model/searchModel.js
@@ -8,7 +8,7 @@ class Search {
     this.RIOT_BASE_URL = "https://kr.api.riotgames.com/lol";
     // https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start={start}&count={count}
   }
-  async myRecod() {
+  async myRecod(callback) {
     const API_URL = `${this.RIOT_BASE_URL}/summoner/v4/summoners/by-name/${this.nickname}?api_key=${this.RIOT_API_KEY}`;
 
     try {
@@ -18,10 +18,22 @@ class Search {
       const matchlistResponse = await axios.get(
         `https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?api_key=${this.RIOT_API_KEY}&start=0&count=5`
       );
-      const { data } = matchlistResponse;
-      console.log(data);
+      const matchId = matchlistResponse.data[0];
+      const m = matchlistResponse.request;
+      const matchGame = await axios.get(
+        `https://asia.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${this.RIOT_API_KEY}`
+      );
+      console.log(Object.entries(matchGame.data.info).length);
     } catch (err) {
-      console.error(err);
+      if (err.response.status == "403") {
+        const errorMessage =
+          "라이엇 api 키가 유효하지 않습니다 api키를 갱신해주세요";
+        callback(errorMessage);
+        return;
+      } else if (err.response.status == "400") {
+        const errorMessage = "전적을 찾을 수 없습니다";
+        callback(errorMessage);
+      }
     }
   }
 }
