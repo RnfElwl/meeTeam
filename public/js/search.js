6deadbd4b339266246e26ecc38abddcf76086569
RnfElwl <90141747+RnfElwl@users.noreply.github.com>
2024-04-28 15:17:12 +0900

    검색 구조 변경 완료!

--- public/js/search.js
+++ public/js/search.js
@@ -2,9 +2,14 @@ const searchForm = document.querySelector(".search_form");
 
 const searchHandle = (e) => {
   e.preventDefault();
-  const inputs = document.querySelectorAll(".search_box");
+
+  const inputs = document.querySelectorAll(".search_box input");
+
+  console.log(inputs[1].value);
   if (!inputs[1].value) {
-    window.location.href = `/searchteam??name=${inputs[0]}`;
+    window.location.href = `/teamSearch?name=${inputs[0].value}`;
+  } else if (inputs[1].value) {
+    window.location.href = `/teamSearch?name=${inputs[1].value}`;
   }
 };
 
